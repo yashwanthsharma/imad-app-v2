97079e5bbbe2291f6d6873c68403252e73dd6556
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-02-15 15:09:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -23,6 +23,35 @@ var articles = {
  }
        
 };
+function createTemplate (data) {
+    var title = data.title;
+    var heading = data.heading;
+    var content = data.content;
+         var htmlTemplate = `
+         <html>
+    <head>
+        <title>Article-one yash!</title>
+         <meta name ="viewport" content ="width=device-width, initial-scale-1" />
+         <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="container">
+            <div>
+              <a href="/">Home</a>
+            </div>
+              <hr/>
+              <h1>ARTICLE-ONE</h1>
+              <div>
+                 <p>
+                    <i>This is my 1st Article</i>
+                 </p>
+              </div>    
+        </div>
+    </body>
+</html>
+`;
+  return htmlTemplate;       
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
