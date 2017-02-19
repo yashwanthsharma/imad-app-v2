83fdfc807a20202a3b6eac587dd001e438e7160a
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-02-19 15:38:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 var element= document.getElementById('main-text');
 
 element.innerHTML='New Value';
+var img = document.getElementById('img');
+img.onclick = function()
+{
+    img.style.marginLeft='100px';
+}
