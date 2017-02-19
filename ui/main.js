71cdc3d0852b9dbdf906a097d4229e7c5aa72915
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-02-19 19:16:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML='New Value by Yash';
 var img = document.getElementById('yash');
 var marginLeft = 0;
 function moveRight (){
- marginLeft = marginLeft + 10;
+ marginLeft = marginLeft + 1;
  img.style.marginLeft = marginLeft + 'px';
 }
 img.onclick = function()
