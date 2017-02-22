1e92a8a6a3ddfdd80b37e9bd91ce86600f38d79e
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-02-22 22:28:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,20 @@
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function(){
-    counter = counter + 1 ;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    // Create a request object
+    var request = new XMLHttpRequest();
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function(){
+        if(rearequest.readyState === XMLHttpRequest.DONE){
+        // take some action
+        if(request.status == 200){
+        var counter = request.respondText;
+        var span = document.getElementById('count');
+        span.innerHTML = counter.toString();
+        }
+     }
+    };
+    
+    request.open('GET','http://yashwanthsharma.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
