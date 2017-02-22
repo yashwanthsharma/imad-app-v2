c02488c1a9fb03fa2a62ed8915c3d33798ca7873
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-02-22 22:09:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,7 @@
-console.log('Loaded!');
-var element= document.getElementById('main-text');
-
-
-var img = document.getElementById('yash');
-var marginLeft = 0;
-function moveRight (){
- marginLeft = marginLeft + 1;
- img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function()
-{
-    var interval = setInterval(moveRight,50);
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    counter = counter + 1 ;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
