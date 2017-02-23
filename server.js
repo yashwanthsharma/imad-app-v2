4bd807402a93583ba3e548e96eaf1b26e2799c66
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-02-23 21:11:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,16 +68,15 @@ app.get('/c/counter',function(req, res){
  counter = counter + 1;
  res.send(counter, toString());
  });
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-
-app.get('/:articleName',function (req, res){
+ app.get('/:articleName',function (req, res){
     //articleName == article-one
     //articles[articleName] == {} content object for article one
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
