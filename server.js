3b75ddf71a41ba3287e8bfaf0b85dcb4a774a428
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-02-23 21:04:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ function createTemplate (data) {
   return htmlTemplate;       
 }
 var counter = 0;
-app.get('/counter',function(req, res){
+app.get('/c/counter',function(req, res){
  counter = counter + 1;
  res.send(counter, toString());
  });
