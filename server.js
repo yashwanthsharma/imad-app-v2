42cb08f5da7cd405fa329a52692df1ab20e7e5cd
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-02-23 21:01:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ var articles = {
  }      
 };
 function createTemplate (data) {
+    
     var title   = data.title;
     var heading = data.heading;
     var content = data.content;
@@ -62,14 +63,15 @@ function createTemplate (data) {
 `;
   return htmlTemplate;       
 }
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
 var counter = 0;
 app.get('/counter',function(req, res){
  counter = counter + 1;
  res.send(counter, toString());
  });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
 
 app.get('/:articleName',function (req, res){
     //articleName == article-one
