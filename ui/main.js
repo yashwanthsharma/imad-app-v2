d7080ad01f213bc011a466d7d56489a761d79935
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-02-25 19:24:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,20 +1,21 @@
 var button = document.getElementById('counter');
-
+var counter = 0;
 button.onclick = function(){
     // Create a request object
-    var request = new XMLHttpRequest();
+    //var request = new XMLHttpRequest();
     //Capture the response and store it in a variable
-    request.onreadyStatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE){
+    //request.onreadyStatechange = function(){
+        //if(request.readyState === XMLHttpRequest.DONE){
         // take some action
-        if(request.status === 200){
-        var counter = request.responseText;
+        //if(request.status === 200){
+        //var counter = request.responseText
+        counter = counter + 1;
         var span = document.getElementById('count');
         span.innerHTML = counter.toString();
-        }
-     }
+      //  }
+    // }
     };
     
-    request.open('GET','http://yashwanthsharma.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
+    //request.open('GET','http://yashwanthsharma.imad.hasura-app.io/counter',true);
+   // request.send(null);
+//};
