e4169eb7c21a18cf381ccc8784b704c5a63809a0
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-02-25 22:53:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
+var names =[];
 app.get('/submit-name/:name', function (req, res) {
  //get the name from the request
   var name = req.params.name ;
