d9360060e19d8f5c9463fb85f63a4b1357dbcadb
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-03-06 11:30:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,9 @@ var config = {
   database: 'yashwanthsharma',
   host:'db.imad.hasura-app.io',
   port:'5432',
-  password: process.env.db-yashwanthsharma-59517
+  password: process.env.DB_PASSWORD
 
-  
-};
+ };
 
 var app = express();
 app.use(morgan('combined'));
@@ -82,7 +81,7 @@ app.get('/', function (req, res) {
 
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
-    pool.query('SELECT*FROM test',function (err, result) {
+    pool.query('SELECT * FROM test',function (err, result) {
        if(err){
            res.status(500).send(err.toString());
        } 
