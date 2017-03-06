b5d24da67f453ac4d20553c9d5491ebd5d310c1a
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-03-06 14:41:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var config = {
   user: 'yashwanthsharma',
   database: 'yashwanthsharma',
   host:'http://db.imad.hasura-app.io',
-  port:'5432',
+  port:'8080',
   password:process.env.DB_PASSWORD
 
  };
