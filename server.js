8b72a8b7c53e1679106738106acd0368deb5d406
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-03-09 19:41:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get('/test-db', function (req, res) {
        } 
        else
        {
-           res.send(JSON.stringfy(result));
+           res.send(JSON.stringify(result));
        }
 });
 
