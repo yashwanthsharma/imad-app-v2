aa5d7d289b56dc1a65c837965441c3ef5188666c
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-03-17 21:08:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.get('/', function (req, res) {
 
 function hash (input, salt) {
     var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return hashed.toString('hex');
+    return ["pbkdf2", "10000", salt, hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input', function(req, res) {
