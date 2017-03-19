6cdc98e20651295a603175b548122835ebcd0676
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-03-19 22:21:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ submit.onclick = function() {
     var password = document.getElementById('password').value;
     console.log(username);
     console.log(password);
-    request.open('POST','http://yashwanthsharma.imad.hasura-app.io/login' + name,true);
+    request.open('POST','http://yashwanthsharma.imad.hasura-app.io/login',true);
     request.setRequestHeader('Content-Type','application/json');
      request.send(JSON.stringify({username:username,password:password}));
     //make a request to the server and send the name
