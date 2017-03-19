69f80cbd1287958a209fb7b5c5044572d2bf0f37
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-03-20 01:47:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto = require('crypto');
 var bodyParser = require('body-parser');
-var session = requires('express-session');
+var session = require('express-session');
 
 var config = {
   
