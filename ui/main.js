1561b0472701dc82fd408079606c24ea2170ec5f
yashwanthsharma <yashwanthsharma916.ys@gmail.com>
2017-03-19 22:17:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,4 @@
-var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function(){
-    // Create a request object
-    
-    
-    var request = new XMLHttpRequest();
-    
-    
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE){
-        // take some action
-          if(request.status === 200){
-            
-          }
-       }
-       // not yet done
-    };
-    // make a request
-     request.open('GET','http://yashwanthsharma.imad.hasura-app.io/counter',true);
-     request.send(null);
- };
-
-//Submit name
+//Submit name username/password to login
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function() {
@@ -36,24 +12,27 @@ submit.onclick = function() {
         // take some action
           if(request.status === 200){
                //Capture a list of names and render it as a list
-            var names = request.responseText;
-            names = JSON.parse(names);
-            var list = '';
-            for (var i=0 ; i<names.length ; i++){
-                list+= '<li>' + names[i] + '<li>';
+            console.log('user logged in');
+            alert('logged in successfully');
+          }
+            else if(request.status === 403){
+                    alert('usename/password is invalid');
                 }
-            var ul = document.getElementById('namelist');
-            ul.innerHTML = list;
-            
+                else if(request.status === 500){
+                        alert('something went wrong');
+                
           }
        }
        // not yet done
     };
     // make a request
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET','http://yashwanthsharma.imad.hasura-app.io/submit-name?name=' + name,true);
-     request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST','http://yashwanthsharma.imad.hasura-app.io/login' + name,true);
+    request.setRequestHeader('Content-Type','application/json');
+     request.send(JSON.stringify({username:username,password:password}));
     //make a request to the server and send the name
     
    
